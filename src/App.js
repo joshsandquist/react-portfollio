c608b5b33dafcade1e87efe44771e5c5da62dab8
Josh Sandquist <joshdsand@gmail.com>
2023-06-15 09:16:13 -0700

    added nav page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react"
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from './components/Header'
 
 const App = () => {
-    <PortfolioContainer />
+    <Header />
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Navigation({ currentPage, setPage }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#aboutme"
+          onClick={() => setPage('About Me')}
+          className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => setPage('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => setPage('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => setPage('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
