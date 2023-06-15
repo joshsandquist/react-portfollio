0fdadc0ba768216aa7fbe1eeb0c216b1046f66d7
Josh Sandquist <joshdsand@gmail.com>
2023-06-14 17:54:48 -0700

    added react component structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from "react"
+import PortfolioContainer from "./components/PortfolioContainer";
 
-
-function App() {
+const App = () => {
+    <PortfolioContainer />
 }
+
 export default App;
